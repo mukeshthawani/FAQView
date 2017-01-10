8525acb65f4845948d182db7b929bcd5493d5d82
Mukesh Thawani <mukesh9039@gmail.com>
2017-01-10 17:53:12 +0530

    Add tests for title label properties

--- Tests/FAQViewTests.swift
+++ Tests/FAQViewTests.swift
@@ -11,13 +11,35 @@ import XCTest
 
 class FAQViewTests: XCTestCase {
   
+  var faqView: FAQView!
+  
   override func setUp() {
     super.setUp()
+    let item = FAQItem(question: "Question", answer: "Answer")
+    faqView = FAQView(frame: CGRect.zero, items: [item])
   }
   
   override func tearDown() {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
+  }
+
+  func test_TitleLabelProperties_WhenSetToDefault() {
+    let titleLabel = faqView.titleLabel
+    let configuration = faqView.configuration
+    XCTAssertEqual(titleLabel?.backgroundColor, configuration.titleLabelBackgroundColor)
+    XCTAssertEqual(titleLabel?.textColor, configuration.titleTextColor)
+    XCTAssertEqual(titleLabel?.font, configuration.titleTextFont)
+  }
+  
+  func test_TitleLabelProperties_WhenCustomized() {
+    let titleLabel = faqView.titleLabel
+    faqView.titleLabelBackgroundColor = UIColor.blue
+    faqView.titleLabelTextColor = UIColor.blue
+    faqView.titleLabelTextFont = UIFont.systemFont(ofSize: 20)
+    XCTAssertEqual(titleLabel?.backgroundColor, UIColor.blue)
+    XCTAssertEqual(titleLabel?.textColor, UIColor.blue)
+    XCTAssertEqual(titleLabel?.font, UIFont.systemFont(ofSize: 20))
   }
   
 }
